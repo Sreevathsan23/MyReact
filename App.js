50f699e6936ac363e40f4e5331afd9dac41db808
SreevathsanNS <vathsan23@gmail.com>
2025-08-09 12:58:16 +0530

    Initial commit

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ const parent = React.createElement(
             React.createElement("h2", {}, "Child H2 tag"),
             React.createElement("h3", {}, "Child H3 tag"),
             React.createElement("h4", {}, "Child H4 tag"),
-            React.createElement("h5", {}, "Child H5 tag"),
+            React.createElement("h5", {}, "Child H5 tag"), 
             React.createElement("h6", {}, "Child H6 tag")
         ]
     )
